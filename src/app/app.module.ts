b2b2828641cc51619761d915084f49bd999658b4
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-19 20:28:43 -0300

    create success alert component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
