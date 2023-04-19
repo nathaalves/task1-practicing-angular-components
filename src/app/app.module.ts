6ff5705aa5501366ba7f3ffc1e9df66ead2e17db
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-19 20:35:31 -0300

    create warning component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SuccessAlertComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SuccessAlertComponent, WarningAlertComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
